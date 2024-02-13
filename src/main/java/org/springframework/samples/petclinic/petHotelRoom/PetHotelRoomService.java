83f20bd87834e9eb52ecdca050a9126064af533f
yato03 <miguelhs3523@gmail.com>
2024-02-13 17:12:06 +0100

    Service y frontend

--- src/main/java/org/springframework/samples/petclinic/petHotelRoom/PetHotelRoomService.java
+++ src/main/java/org/springframework/samples/petclinic/petHotelRoom/PetHotelRoomService.java
@@ -1,5 +1,21 @@
 package org.springframework.samples.petclinic.petHotelRoom;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PetHotelRoomService {
     
+    private PetHotelRoomRepository petHotelRoomRepository;
+
+    public PetHotelRoomService(PetHotelRoomRepository petHotelRoomRepository) {
+        this.petHotelRoomRepository = petHotelRoomRepository;
+    }
+    public void savePetHotelRoom(PetHotelRoom petHotelRoom) {
+        petHotelRoomRepository.save(petHotelRoom);
+    }
+
+    public List<PetHotelRoom> findAll() {
+        return petHotelRoomRepository.findAll();
+    }
+
 }
