206078a019cd4a4d0c59e7bc009862f7d2b439f5
Davgodfer <davgodfer@alum.us.es>
2024-04-29 00:05:49 +0200

    Actualización

--- frontend/src/public/plan/index.js
+++ frontend/src/public/plan/index.js
@@ -1,10 +1,4 @@
 import React from "react";
-import { BsDot, BsFillRocketTakeoffFill } from "react-icons/bs";
-import { FaCheck, FaPaperPlane, FaTimes } from "react-icons/fa";
-import { PiCrownSimpleFill } from "react-icons/pi";
-import { IoDiamond } from "react-icons/io5";
-import { TbCurrencyDollarOff } from "react-icons/tb";
-import { ImAirplane } from "react-icons/im";
 import "../../static/css/pricing/pricingPage.css";
 
 export default function PlanList() {
@@ -12,10 +6,8 @@ export default function PlanList() {
   const planList = [
     {
       plan: "BASIC",
-      price: "0.00",
-      currency: "€",
-      frecuency: "/MONTH",
       features: [
+        { feature: "Price", value: "0.00€/MONTH" },
         { feature: "Number of Pets", value: 2 },
         { feature: "Visits per Month and Pet", value: 1 },
         { feature: "Support Priority", value: "Low" },
@@ -23,18 +15,15 @@ export default function PlanList() {
         { feature: "Adoptions Service", value: "❌" },
         { feature: "Vet Selection for Visits", value: "❌" },
         { feature: "Calendar with Upcoming Visits", value: "❌" },
-        { feature: "Pet Hotel Room Creation", value: "❌" },
-        { feature: "Booking Creation", value: "❌" },
-        { feature: "Dashboard of Your Pets", value: "❌" },
+        { feature: "Pet Hotel Room Service", value: "❌" },
+        { feature: "Booking Service", value: "❌" },
         { feature: "Online Consultation", value: "❌" }
       ]
     },
     {
       plan: "GOLD",
-      price: "5.00",
-      currency: "€",
-      frecuency: "/MONTH",
       features: [
+        { feature: "Price", value: "5.00€/MONTH" },
         { feature: "Number of Pets", value: 5 },
         { feature: "Visits per Month and Pet", value: 3 },
         { feature: "Support Priority", value: "Medium" },
@@ -42,18 +31,15 @@ export default function PlanList() {
         { feature: "Adoptions Service", value: "✅" },
         { feature: "Vet Selection for Visits", value: "✅" },
         { feature: "Calendar with Upcoming Visits", value: "✅" },
-        { feature: "Pet Hotel Room Creation", value: "❌" },
-        { feature: "Booking Creation", value: "❌" },
-        { feature: "Dashboard of Your Pets", value: "❌" },
+        { feature: "Pet Hotel Room Service", value: "❌" },
+        { feature: "Booking Service", value: "❌" },
         { feature: "Online Consultation", value: "❌" }
       ]
     },
     {
       plan: "PLATINUM",
-      price: "12.00",
-      currency: "€",
-      frecuency: "/MONTH",
       features: [
+        { feature: "Price", value: "12.00€/MONTH" },
         { feature: "Number of Pets", value: 12 },
         { feature: "Visits per Month and Pet", value: 6 },
         { feature: "Support Priority", value: "High" },
@@ -61,9 +47,8 @@ export default function PlanList() {
         { feature: "Adoptions Service", value: "✅" },
         { feature: "Vet Selection for Visits", value: "✅" },
         { feature: "Calendar with Upcoming Visits", value: "✅" },
-        { feature: "Pet Hotel Room Creation", value: "✅" },
-        { feature: "Booking Creation", value: "✅" },
-        { feature: "Dashboard of Your Pets", value: "✅" },
+        { feature: "Pet Hotel Room Service", value: "✅" },
+        { feature: "Booking Service", value: "✅" },
         { feature: "Online Consultation", value: "✅" }
       ]
     }
@@ -81,18 +66,12 @@ export default function PlanList() {
                   <th className="pricing-table-feature">Feature</th>
                     <th className="pricing-table-free">
                       {planList[0].plan}
-                      <br />
-                      {planList[0].price}{planList[0].currency}{planList[0].frecuency}
                       </th>
                       <th className="pricing-table-gold">
                       {planList[1].plan}
-                      <br />
-                      {planList[1].price}{planList[1].currency}{planList[1].frecuency}
                       </th>
                       <th className="pricing-table-platinum">
                       {planList[2].plan}
-                      <br />
-                      {planList[2].price}{planList[2].currency}{planList[2].frecuency}
                       </th>
                 </tr>
               </thead>
