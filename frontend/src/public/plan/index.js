ff02542bec34dda2bf8c76fbd05b5df1ac2d1e3a
Davgodfer <davgodfer@alum.us.es>
2024-04-28 21:32:44 +0200

    #63 Pricing

--- frontend/src/public/plan/index.js
+++ frontend/src/public/plan/index.js
@@ -8,131 +8,107 @@ import { ImAirplane } from "react-icons/im";
 import "../../static/css/pricing/pricingPage.css";
 
 export default function PlanList() {
-  
-    return (
-      <div className="pricing-page-container">
-        <div>
-          <h1 className="pricing-title">Pricing Plans</h1>
-        </div>
-        <div className="section-pricing">
-          <div className="pricing-container">
-            <div className="pricing-card text-center">
-              <div className="title">
-                <div className="icon">
-                  <TbCurrencyDollarOff color="white" />
-                </div>
-                <h2>BASIC</h2>
-              </div>
-              <div className="plan-price">
-                <h4>FREE</h4>
-              </div>
-              <div className="option">
-                <ul>
-                  <li>
-                    <BsDot color="white" /> 2 pets
-                  </li>
-                  <li>
-                    <BsDot color="white" /> 1 visit per month and pet
-                  </li>
-                  <li>
-                    <BsDot color="white" /> Low support priority
-                  </li>
-                  <li>
-                    <FaTimes color="red" /> Vet Selection for Visits
-                  </li>
-                  <li>
-                    <FaTimes color="red" /> Calendar with Upcoming Visits
-                  </li>
-                  <li>
-                    <FaTimes color="red" /> Dashboard of your Pets
-                  </li>
-                  <li>
-                    <FaTimes color="red" /> Online Consultation
-                  </li>
-                </ul>
-              </div>
-            </div>
-            {/* END Col one */}
-            <div className="pricing-card text-center">
-              <div className="title">
-                <div className="icon">
-                  <PiCrownSimpleFill color="white" style={{ transform: "rotate(270deg)" }} />
-                </div>
-                <h2>GOLD</h2>
-              </div>
-              <div className="plan-price">
-                <h4>5</h4>
 
-                <h5>€</h5>
-              </div>
-              <div className="option">
-                <ul>
-                  <li>
-                    <BsDot color="white" /> 4 pets
-                  </li>
-                  <li>
-                    <BsDot color="white" /> 3 visit per month and pet
-                  </li>
-                  <li>
-                    <BsDot color="white" /> Medium support priority
-                  </li>
-                  <li>
-                    <FaCheck color="green" /> Vet Selection for Visits
-                  </li>
-                  <li>
-                    <FaCheck color="green" /> Calendar with Upcoming Visits
-                  </li>
-                  <li>
-                    <FaTimes color="red" /> Dashboard of your Pets
-                  </li>
-                  <li>
-                    <FaTimes color="red" /> Online Consultation
-                  </li>
-                </ul>
-              </div>
-            </div>
-            {/* END Col two */}
-            <div className="pricing-card text-center">
-              <div className="title" style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                <div className="icon">
-                  <IoDiamond color="white" />
-                </div>
-                <h2>PLATINUM</h2>
-              </div>
-              <div className="plan-price">
-                <h4>12</h4>
+  const planList = [
+    {
+      plan: "BASIC",
+      price: "0.00",
+      currency: "€",
+      frecuency: "/MONTH",
+      features: [
+        { feature: "Number of Pets", value: 2 },
+        { feature: "Visits per Month and Pet", value: 1 },
+        { feature: "Support Priority", value: "Low" },
+        { feature: "Include SLA", value: "No" },
+        { feature: "Adoptions Service", value: "❌" },
+        { feature: "Vet Selection for Visits", value: "❌" },
+        { feature: "Calendar with Upcoming Visits", value: "❌" },
+        { feature: "Pet Hotel Room Creation", value: "❌" },
+        { feature: "Booking Creation", value: "❌" },
+        { feature: "Dashboard of Your Pets", value: "❌" },
+        { feature: "Online Consultation", value: "❌" }
+      ]
+    },
+    {
+      plan: "GOLD",
+      price: "5.00",
+      currency: "€",
+      frecuency: "/MONTH",
+      features: [
+        { feature: "Number of Pets", value: 5 },
+        { feature: "Visits per Month and Pet", value: 3 },
+        { feature: "Support Priority", value: "Medium" },
+        { feature: "Include SLA", value: "Yes" },
+        { feature: "Adoptions Service", value: "✅" },
+        { feature: "Vet Selection for Visits", value: "✅" },
+        { feature: "Calendar with Upcoming Visits", value: "✅" },
+        { feature: "Pet Hotel Room Creation", value: "❌" },
+        { feature: "Booking Creation", value: "❌" },
+        { feature: "Dashboard of Your Pets", value: "❌" },
+        { feature: "Online Consultation", value: "❌" }
+      ]
+    },
+    {
+      plan: "PLATINUM",
+      price: "12.00",
+      currency: "€",
+      frecuency: "/MONTH",
+      features: [
+        { feature: "Number of Pets", value: 12 },
+        { feature: "Visits per Month and Pet", value: 6 },
+        { feature: "Support Priority", value: "High" },
+        { feature: "Include SLA", value: "Yes" },
+        { feature: "Adoptions Service", value: "✅" },
+        { feature: "Vet Selection for Visits", value: "✅" },
+        { feature: "Calendar with Upcoming Visits", value: "✅" },
+        { feature: "Pet Hotel Room Creation", value: "✅" },
+        { feature: "Booking Creation", value: "✅" },
+        { feature: "Dashboard of Your Pets", value: "✅" },
+        { feature: "Online Consultation", value: "✅" }
+      ]
+    }
+  ];
 
-                <h5>€</h5>
-              </div>
-              <div className="option">
-                <ul>
-                  <li>
-                    <BsDot color="white" /> 7 pets
-                  </li>
-                  <li>
-                    <BsDot color="white" /> 6 visit per month and pet
-                  </li>
-                  <li>
-                    <BsDot color="white" /> High support priority
-                  </li>
-                  <li>
-                    <FaCheck color="green" /> Vet Selection for Visits
-                  </li>
-                  <li>
-                    <FaCheck color="green" /> Calendar with Upcoming Visits
-                  </li>
-                  <li>
-                    <FaCheck color="green" /> Dashboard of your Pets
-                  </li>
-                  <li>
-                    <FaCheck color="green" /> Online Consultation
-                  </li>
-                </ul>
-              </div>
-            </div>
-            {/* END Col three */}
+
+  return (
+      <div className="section-pricing">
+        <div className="pricing-container">
+          <div className="pricing-table th">
+            <h1>Pricing Plans</h1>
+            <table className="pricing-table"> {/* Added className */}
+              <thead>
+                <tr>
+                  <th className="pricing-table-feature">Feature</th>
+                    <th className="pricing-table-free">
+                      {planList[0].plan}
+                      <br />
+                      {planList[0].price}{planList[0].currency}{planList[0].frecuency}
+                      </th>
+                      <th className="pricing-table-gold">
+                      {planList[1].plan}
+                      <br />
+                      {planList[1].price}{planList[1].currency}{planList[1].frecuency}
+                      </th>
+                      <th className="pricing-table-platinum">
+                      {planList[2].plan}
+                      <br />
+                      {planList[2].price}{planList[2].currency}{planList[2].frecuency}
+                      </th>
+                </tr>
+              </thead>
+              <tbody>
+                {planList[0].features.map((feature, index) => (
+                  <tr key={index}>
+                    <td>{feature.feature}</td>
+                    {planList.map((plan, idx) => (
+                      <td key={idx}>{plan.features[index].value}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    );
-  }
+  );
+}
